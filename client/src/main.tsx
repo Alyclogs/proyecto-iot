59f7c18bd36310cabd49fc7a1fb6a2efaff18313
Alyclogs <meatychip.coolbaby@gmail.com>
2025-06-22 16:46:03 -0500

    Sin comentarios

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,36 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css"; // Estilos globales
+import "./index.css";
 import { ThemeProvider, createTheme, alpha } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
-// Paleta de colores suaves
 const softPalette = {
   primary: {
-    main: "#79A7D3", // Azul suave
+    main: "#79A7D3",
     light: alpha("#79A7D3", 0.8),
     dark: alpha("#79A7D3", 0.6),
     contrastText: "#ffffff",
   },
   secondary: {
-    main: "#A0D2DB", // Turquesa muy suave
-    light: alpha("#A0D2DB", 0.8), // Añadido para evitar el error
+    main: "#A0D2DB",
+    light: alpha("#A0D2DB", 0.8),
     contrastText: "#263238",
   },
   background: {
-    default: "#F0F4F8", // Fondo general muy claro, casi blanco azulado
-    paper: "#FFFFFF", // Fondo de los 'Paper' componentes
+    default: "#F0F4F8",
+    paper: "#FFFFFF",
   },
   text: {
-    primary: "#333B4F", // Gris oscuro azulado para texto principal
-    secondary: "#6B7A90", // Gris más claro para texto secundario
+    primary: "#333B4F",
+    secondary: "#6B7A90",
   },
   error: {
-    main: "#E57373", // Rojo suave para errores
+    main: "#E57373",
   },
   success: {
-    main: "#81C784", // Verde suave para éxito
+    main: "#81C784",
   },
 };
 
@@ -59,17 +58,16 @@ const theme = createTheme({
           // padding: 7,
         },
         switchBase: {
-          // Estilos para el switch cuando está desactivado
-          color: softPalette.secondary.main, // Color del thumb cuando está apagado
+          color: softPalette.secondary.main,
           "&.Mui-checked": {
-            color: softPalette.primary.main, // Color del thumb cuando está encendido
+            color: softPalette.primary.main,
             "& + .MuiSwitch-track": {
-              backgroundColor: softPalette.primary.light, // Color del track cuando está encendido
+              backgroundColor: softPalette.primary.light,
             },
           },
         },
         track: {
-          backgroundColor: softPalette.secondary.light, // Color del track cuando está apagado
+          backgroundColor: softPalette.secondary.light,
         },
       },
     },
