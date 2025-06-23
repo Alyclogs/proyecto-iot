fa2f80ae70a9dd16f9f290c779af8c7380ad1ed5
Alyclogs <meatychip.coolbaby@gmail.com>
2025-06-22 21:20:34 -0500

    Fixing vercel config 4

--- client/src/components/LedControl.tsx
+++ client/src/components/LedControl.tsx
@@ -35,7 +35,7 @@ interface CommandResponse {
 }
 const sendLedCommand = async (state: boolean): Promise<CommandResponse> => {
   try {
-    const response = await fetch("/api/light.ts", {
+    const response = await fetch("/api/src/light", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
